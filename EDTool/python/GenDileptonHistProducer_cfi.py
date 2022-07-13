3da035d2d819cab879f4977a34419fe13656bfbe
kplee <kplee@cern.ch>
2022-07-13 17:23:57 +0200

    fix errors

--- EDTool/python/GenDileptonHistProducer_cfi.py
+++ EDTool/python/GenDileptonHistProducer_cfi.py
@@ -4,7 +4,6 @@ GenDileptonHistProducer = cms.EDAnalyzer('GenDileptonHistProducer',
     GenEventInfo = cms.untracked.InputTag("generator"),
     GenParticles = cms.untracked.InputTag("genParticles"),
 
-    genID_lepton    = cms.untracked.int(13),
-    # -- isHardProcess or fromHardProcessFinalState
-    genFlag_lepton  = cms.untracked.string("isHardProcess"),
+    genID_lepton   = cms.untracked.int32(13),
+    genFlag_lepton = cms.untracked.string("isHardProcess"), # -- isHardProcess or fromHardProcessFinalState
 )
